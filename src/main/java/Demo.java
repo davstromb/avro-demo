2a2d0bcfd72b9c95cf3a0ba06e8853bfb8834f4b
davstromb <davstromb@gmail.com>
2017-11-09 15:52:43 +0100

    init

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -13,8 +13,13 @@ public class Demo {
     public Demo() throws IOException {
 
         // CREATE AN OBJECT
+        User user = User.newBuilder()
+                .setName("David")
+                .setFavouriteColor("Yellow")
+                .setFavouriteNumber(22)
+                .build();
 
-
+        System.out.println("My user: " + user);
 
 
         // CREATE OBJECT WITH REFLECTION
