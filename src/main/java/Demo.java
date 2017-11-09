08ece371c78aeddc7432a5a69b68a9385bde5285
davstromb <davstromb@gmail.com>
2017-11-09 15:47:00 +0100

    base

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,50 +1,47 @@
-import avro.user.User;
-import org.apache.avro.file.DataFileWriter;
-import org.apache.avro.generic.GenericDatumWriter;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.specific.SpecificDatumWriter;
-
 import java.io.File;
 import java.io.IOException;
+import avro.*;
 
 public class Demo {
 
 
     public static void main(String[] args) throws IOException {
-        System.out.println("\nData Serialization w/ Apache Avro - Demo (Lol)\n\n");
+        System.out.println("\n");
         new Demo();
     }
 
     public Demo() throws IOException {
 
-        // SERIALIZE
-//        User marieDyer = User.newBuilder()
-//                .setName("Marie Dyer")
-//                .setFavouriteNumber(8)
-//                .build();
+        // CREATE AN OBJECT
 
 
-        User user = new User();
-        user.setName("Marie Dyer");
-        user.put("name", "Marie Dyer");
-        user.put("favourite_color", "Blue");
-        user.setFavouriteNumber(8);
+
+
+        // CREATE OBJECT WITH REFLECTION
+
+
+
 
         // SERIALIZE TO DISK
-        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
-        DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
-        dataFileWriter.create(user.getSchema(), new File("users_lulz.avro"));
-        dataFileWriter.append(user);
-        dataFileWriter.flush();
-        dataFileWriter.close();
-
-        // DESERIALIZE
 
 
 
 
-        System.out.println(user);
+        // DESERIALIZE FROM DISK
+
+
+
+        // SCHEMA EVOLUTION
+
+
+    }
+
+    private File getFile(final String filename) {
+        return new File(Thread
+                .currentThread()
+                .getContextClassLoader()
+                .getResource("data/" + filename)
+                .getPath());
     }
 
 
